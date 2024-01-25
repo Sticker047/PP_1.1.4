5de564322ae301dd1aa3956dc095d0df183d882d
I.Gagilev <gagileww2@gmail.com>
2024-01-25 13:03:34 +0500

    Тесты пройдены успешно

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,8 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.ResultSet;
@@ -10,20 +12,21 @@ import java.sql.Statement;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
         // реализуйте алгоритм здесь
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
-        userDaoJDBC.createUsersTable();
+        UserService userService = new UserServiceImpl(new UserDaoJDBCImpl());
 
-        userDaoJDBC.saveUser("Иван", "Иванов", (byte) 20);
-        userDaoJDBC.saveUser("Петр", "Петров", (byte) 20);
-        userDaoJDBC.saveUser("Сидор", "Сидоров", (byte) 20);
-        userDaoJDBC.saveUser("Василий", "Васечкин", (byte) 20);
+        userService.createUsersTable();
 
-        userDaoJDBC.getAllUsers().forEach(System.out::println);
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.saveUser("Иван", "Иванов", (byte) 20);
+        userService.saveUser("Петр", "Петров", (byte) 20);
+        userService.saveUser("Сидор", "Сидоров", (byte) 20);
+        userService.saveUser("Василий", "Васечкин", (byte) 20);
+
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
